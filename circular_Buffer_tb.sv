b7d03b457fafb608edf27aac2d43776f5a7581f3
Rahim Shaik <aashiq.sk444@gmail.com>
2024-02-14 22:58:14 -0600

    removed the struct test files and refined the test bench

--- circular_Buffer_tb.sv
+++ circular_Buffer_tb.sv
@@ -19,17 +19,27 @@ module circular_Buffer_tb #(parameter BUFFER_SIZE = 8);
   logic buf_full;
   logic buf_On_Off;
 
-  
+ //clock generation  
+  always #1 clk = ~clk; 
+ 
+initial begin 
+
+clk = 1'b1;
+
+end 
   initial begin 
-    //output_VCD();
     initialize();
     createflit();
+    @(posedge clk);
     rst_n = 1'b1;
     @(posedge clk);
     write_i = 1'b1;
     input_Data = new_Flit;
     @(posedge clk);
+    write_i = 1'b0;
     createflit();
+    @(posedge clk);
+    write_i = 1'b1;
     input_Data = new_Flit;
     @(posedge clk);
     write_i = 1'b0;
@@ -41,30 +51,18 @@ module circular_Buffer_tb #(parameter BUFFER_SIZE = 8);
     $finish();
   end 
 
-  //clock generation  
-  always #5 clk = ~clk;
+ 
 
   circular_Buffer #(.BUFFER_SIZE(BUFFER_SIZE)) cirbuf (.clk(clk), .rst_n(rst_n), .input_Data(input_Data), .output_Data(output_Data), .read_i(read_i), .write_i(write_i),
   .buf_empty(buf_empty), .buf_full(buf_full), .buf_On_Off(buf_On_Off));
 
-  
-
-  //task output_VCD();
-    //this is the file where simulation output will be dumped during the simulation
-    //$dumpfile ("circ_Buff_Out.vcd");
-    //dump all the variables in the top level module here 0 represents the dumping mode i.e, it dumps the current values of the specified variables.
-    //1: Dumps the changes to the specified variables since the last dump operation.
-    //$dumpvars (0, circular_Buffer_tb);
-    //for(i=0; i<BUFFER_SIZE; i++)
-        //$dumpvars (0, cirbuf.memory[i]);
-    //i=0;  
-  //endtask
 
   task initialize();
-    clk     = 1'b1;
     rst_n   = 1'b0;
     i	    = 0;
     op_Num  = 0;
+    write_i = 0;
+    read_i  = 0;
   endtask
 
   task createflit();
