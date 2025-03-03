58974d8729dbdeeab6b66aae899bb695eea45b06
Rahim Shaik <aashiq.sk444@gmail.com>
2025-03-02 20:51:25 -0600

    tb

--- in_out_allocator_tb.sv
+++ in_out_allocator_tb.sv
@@ -5,7 +5,7 @@ module in_out_allocator_tb#(parameter vc_Num =4);
 
     logic clk, rst_n;                                               //clock, reset
     logic [in_Port_Cnt-1:0][vc_Num-1:0] request_in;                 //input port
-    port_t [vc_Num-1:0] out_port_cmd [in_Port_Cnt-1:0];             //output port
+    inout_Port [vc_Num-1:0] out_port_cmd [in_Port_Cnt-1:0];             //output port
     localparam [31:0] AGENTS_PTR_SIZE_IN = $clog2(vc_Num);          //
     localparam [31:0] AGENTS_PTR_SIZE_OUT = $clog2(in_Port_Cnt);
     logic [in_Port_Cnt-1:0][AGENTS_PTR_SIZE_IN-1:0] curr_highest_priority_vc, next_highest_priority_vc;
@@ -17,5 +17,8 @@ module in_out_allocator_tb#(parameter vc_Num =4);
     wire [in_Port_Cnt-1:0][vc_Num-1:0] grants;
     inout_Port [in_Port_Cnt-1:0] ports = {LOCAL, NORTH, SOUTH, WEST, EAST};
 
+  always #5 clk= ~clk;
+
+  in_out_allocator(.vc_Num(vc_Num)) iobuff (.clk(clk), .rst_n(rst_n), .request_in(request_in), .inports_Out(out_port_cmd), .grant_o(grants);)
 endmodule
 
