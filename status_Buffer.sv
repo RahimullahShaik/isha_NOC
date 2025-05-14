ff5f83dab9a872386bebbb4e117da5c0a874294e
Rahim Shaik <aashiq.sk444@gmail.com>
2025-05-13 22:51:06 -0500

    Wrote the logic for IDLE & VC states

--- status_Buffer.sv
+++ status_Buffer.sv
@@ -15,7 +15,7 @@ module status_Buffer #(parameter BUFFER_SIZE = 8)
   output flit_Data_withvc output_Data,		        //output flit after being assigned to a vc 
   input inout_Port port_i,			                  //input port
   output inout_Port port_o,			                  //output port for the next hop we get the next hop info from the Route computation block 
-  input [VC_Size-1:0] vc_New,			                //Vc allocation 
+  input [VC_Size-1:0] vc_New,			                //New vc available at the downstream router for allocation 
   input vc_Val,					                          //Vc valid 
   output logic vc_Req,				                    //Vc allocationn request
   output logic vc_Alloc,			                    //Vc allocation for the next flit sent to the upstream router 
@@ -57,22 +57,26 @@ end
     
 
 //Circular Buffer instantiation
-circular_Buffer #(.BUFFER_SIZE(BUFFER_SIZE)) cirbuf (.clk(clk), .rst_n(rst_n), .input_Data(input_Data), .output_Data(output_Data), .read_i(read_i), .write_i(write_i),
+circular_Buffer #(.BUFFER_SIZE(BUFFER_SIZE)) cirbuf (.clk(clk), .rst_n(rst_n), .input_Data(input_Data), .output_Data(output_Data), .read_i(read_Cmd), .write_i(write_Cmd),
   .buf_empty(buf_empty), .buf_full(buf_full), .buf_On_Off(buf_On_Off));
 
 // //FSM logic 
  always@(*) begin 
    nxt_state = p_state;
    downstream_Vc_Next = downstream_Vc;
-   read_i = 0
-   write_i = 0
-   end_Packet_Next = end_Packet
+   read_Cmd = 0;
+   write_Cmd = 0;
+   end_Packet_Next = end_Packet;
+   vc_Alloc_next = 0;
+   switch_Req = 0;
+   port_o_Next = port_o;
+   err_Next = 0; 
+
 
 
   case(p_state)
     IDLE: begin
-      //if buffer is empty and the transaction is write and only if the flit is head or headtail, we should allocate it in buffer
-      //the next hop output port is loaded  
+      //if buffer is empty and the transaction is write and only if the flit is head or headtail, we should be in IDLE state and allocate the data into the circular buffer
       if((input_Data.flit_Data_Label = HEAD | input_Data.flit_Data_Label = HEADTAIL) & write_i & buf_empty)begin 
         nxt_state = VC;
         port_o_Next = port_o;
@@ -80,17 +84,26 @@ circular_Buffer #(.BUFFER_SIZE(BUFFER_SIZE)) cirbuf (.clk(clk), .rst_n(rst_n), .
       end 
       //if the flit is Head's tail it means flit should end this is the last beat
       if(write_i & input_Data.flit_Data_Label = HEADTAIL) begin
-        end_Packet = 1
+        end_Packet = 1;
       end
 
+      //if vc_Val gets asserted which should ideally get asserted in vc allocation state or if we are trying to read from the buffer or if the buffer is empty or if the 
+      //flits are Body and tail then err should be asserted.
+      if(vc_Val | read_i | ~buf_empty | ((input_Data.flit_Data_Label = BODY | input_Data.flit_Data_Label = TAIL) & write_i)) begin
+        err_Next = 1;
+      end
+          end
 
-        end 
+    //start the vc allocation and wait for the body and tail packets to arrive  
      VC: begin 
+      vc_Req = 1;                   //sending vc allocation request to vc allocator module to get the next available free vc at the downstream router
+      if() 
+      if(input_Data.flit_Data_Label = HEAD)
+        err = 1;
+      end  
+      if
+    SWITCH: begin 
       end 
-//     VC: begin 
-//     end 
-//     SWITCH: begin 
-//     end 
    endcase
  end
 
